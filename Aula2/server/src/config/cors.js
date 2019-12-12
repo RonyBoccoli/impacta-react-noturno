662e0ff0ed9abf4f47f4a666db9712a94c35f126
no5120@CLASSROOM.NET <no5120@CLASSROOM.NET>
2019-12-11 22:40:35 -0300

    aula web service api2

--- Aula2/server/src/config/cors.js
+++ Aula2/server/src/config/cors.js
@@ -1,4 +1,4 @@
-module.exports = (_, respose,next) =>{
+module.exports = (_, response,next) =>{
     response.header("Access-Control-Allow-Origin", "*");
     response.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
     response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
